8a439c155de3100fa215bd9458dc4ce1481cfc97
Tobias, Sachse <tobisachse27@msn.com>
2022-02-13 22:20:26 +0100

    Unity_Exercise

---
+++ test/LedDriverTest.c
@@ -0,0 +1,61 @@
+/***
+ * Excerpted from "Test-Driven Development for Embedded C",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/jgade for more book information.
+***/
+/*- ------------------------------------------------------------------ -*/
+/*-    Copyright (c) James W. Grenning -- All Rights Reserved          -*/
+/*-    For use by owners of Test-Driven Development for Embedded C,    -*/
+/*-    and attendees of Renaissance Software Consulting, Co. training  -*/
+/*-    classes.                                                        -*/
+/*-                                                                    -*/
+/*-    Available at http://pragprog.com/titles/jgade/                  -*/
+/*-        ISBN 1-934356-62-X, ISBN13 978-1-934356-62-3                -*/
+/*-                                                                    -*/
+/*-    Authorized users may use this source code in your own           -*/
+/*-    projects, however the source code may not be used to            -*/
+/*-    create training material, courses, books, articles, and         -*/
+/*-    the like. We make no guarantees that this source code is        -*/
+/*-    fit for any purpose.                                            -*/
+/*-                                                                    -*/
+/*-    www.renaissancesoftware.net james@renaissancesoftware.net       -*/
+/*- ------------------------------------------------------------------ -*/
+
+#include "unity_fixture.h"
+#include "LedDriver.h"
+
+#include "unity_fixture.h"
+
+TEST_GROUP(LedDriver);
+
+static uint16_t virtualLeds;
+
+TEST_SETUP(LedDriver)
+{
+    virtualLeds = 0xffff;
+}
+
+TEST_TEAR_DOWN(LedDriver)
+{
+}
+
+TEST(LedDriver, LedDriver_CreateOff)
+{
+    LedDriver_Create(&virtualLeds);
+    TEST_ASSERT_EQUAL_HEX16(0x00, virtualLeds);
+}
+
+TEST(LedDriver, TurnOn_LedOne)
+{
+    LedDriver_LedOn(1);
+    TEST_ASSERT_EQUAL_HEX16(0x01, virtualLeds);
+}
+
+TEST(LedDriver, TurnOff_LedOne)
+{
+    LedDriver_LedOff(1);
+    TEST_ASSERT_EQUAL_HEX16(0x00, virtualLeds);
+}
